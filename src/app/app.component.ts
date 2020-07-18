da36170e61576fad8878db6d3e7e9fb19198c59c
Muzaffar Munir <muzafarmunir@gmail.com>
2020-07-18 23:20:26 +0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+  imageObject: Array<object> = [] as any;
   typeTitle = 'SEE IT';
   title = 'Vysion';
   active = 'active';
@@ -14,7 +15,24 @@ export class AppComponent {
     this.typeTitle = type;
   }
 
-  uploadFile(file) {
-    console.log(file);
+  uploadFile(event: any): any {
+    console.log(event);
+    if (event.target.value) {
+      const objVideo = {
+        // video: event.target.value,
+        video: 'https://youtu.be/6pxRHBw-k8M'
+      };
+      const objImage = {
+        image: event.target.value,
+        thumbImage: event.target.value,
+      };
+      this.imageObject.push(objImage);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        console.log(reader.result);
+      };
+      reader.readAsDataURL(event.target.value);
+      console.log(reader.readAsDataURL(event.target.value));
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatIconModule} from '@angular/material/icon';
+import { NgImageSliderModule } from 'ng-image-slider';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule,
+    NgImageSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
